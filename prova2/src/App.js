1ad683757b9d6ab6c3b0c82ec252227caa561bb3
Mauricio-Peres <maumaumperes@gmail.com>
2025-06-10 21:53:57 -0300

    Add files via upload

---
+++ prova2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ConnectBook</h1> 
+      <p>Início Contatos</p>
+      
+      
+    </div>
+  );
+}
+
+export default App;
